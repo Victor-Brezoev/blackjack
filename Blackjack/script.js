584a9d879d86033b5fe8692e32eb2639830774bd
Victor-Brezoev <101119589+Victor-Brezoev@users.noreply.github.com>
2022-03-10 16:04:35 +0200

    Add files via upload

---
+++ Blackjack/script.js
@@ -0,0 +1,63 @@
+let messageEl = document.getElementById('message-el')
+let cardsEl = document.getElementById('cards-el')
+let sumEl = document.getElementById('sum-el')
+let playerEl = document.getElementById('player-el')
+let cards = []
+let sum = 0
+let message = ''
+let hasBlackJack = false
+let gameOn = false
+let player = {
+  name : 'Victor',
+  chips: 100
+}
+
+playerEl.textContent =  player.name + ": $" + player.chips
+
+function getRandomCard() {
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
+
+function  startGame() {
+   gameOn = true
+  let firstCard = getRandomCard()
+  let secondCard = getRandomCard()
+  cards = [firstCard, secondCard]
+  sum = firstCard + secondCard
+  renderGame()
+}
+
+function renderGame(){
+  cardsEl.textContent = "Cards: "
+  for ( i = 0 ; i < cards.length ; i++) {
+    cardsEl.textContent += cards[i] + " "
+  }
+  sumEl.textContent = "Sum: " + sum
+  if (sum <= 20) {
+    message = " Do you want another card?"
+  } else if (sum === 21) {
+    message = "You Win!"
+    hasBlackJack = true
+  } else {
+    message = "Loser"
+    gameOn = false
+  }
+  messageEl.textContent = message
+}
+
+
+function newCard (){
+  if ( gameOn === true && hasBlackJack === false){
+    let card = getRandomCard()
+    sum += card
+    cards.push(card)
+    renderGame()
+  }
+}
